41b80ae177ccb220192ebfca4b776f1f45d3ff9e
kaimuell <info@kaimueller-art.de>
2024-01-09 12:34:10 +0100

    Code for a test Suite

---
+++ main.py
@@ -0,0 +1,47 @@
+from testSuite import TestSuite
+from testcase import WasRun, TestCase, TestResult
+
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert "setUp testMethod tearDown " == test.log
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert "1 run, 0 failed" == self.result.summary()
+
+    def testFailedResult(self):
+        test=WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert "1 run, 1 failed" == self.result.summary()
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert "1 run, 1 failed" == result.summary()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert ("2 run, 1 failed") == self.result.summary()
+
+
+if __name__ == '__main__':
+    suite = TestSuite()
+    suite.add(TestCaseTest("testTemplateMethod"))
+    suite.add(TestCaseTest("testResult"))
+    suite.add(TestCaseTest("testFailedResultFormatting"))
+    suite.add(TestCaseTest("testFailedResult"))
+    suite.add(TestCaseTest("testSuite"))
+    result = TestResult()
+    suite.run(result)
+    print(result.summary())
---
+++ testcase.py
@@ -0,0 +1,56 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+        self.wasSetUp = None
+
+    def setUp(self):
+        pass
+
+    def run(self, result):
+        result.testStarted()
+        self.setUp()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
+
+
+class TestResult():
+
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+    def testBrokenMethod(self):
+        raise Exception
